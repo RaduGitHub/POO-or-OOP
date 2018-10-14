87f4e0ffeefc121a1424914932b9d2710422e323
RaduGitHub <radu.mihai17@yahoo.com>
2018-10-14 22:06:47 +0300

    Problem done

---
+++ IMAGINARY_NUMBER/fivide.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+struct comp {
+    float real, imaginary;
+};
+
+void divide(float real_1, float imag_1, float real_2, float imag_2);
---
+++ IMAGINARY_NUMBER/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fivide.h"
+
+int main(){
+
+    struct comp no1, no2;
+
+    printf("Real part first number: ");
+    scanf("%f", &no1.real);
+    printf("\nImaginary part first number: ");
+    scanf("%f", &no1.imaginary);
+
+    printf("\nReal part second number: ");
+    scanf("%f", &no2.real);
+    printf("\nImaginary part second number: ");
+    scanf("%f", &no2.imaginary);
+
+    divide(no1.real, no1.imaginary, no2.real, no2.imaginary);
+
+    return 0;
+}
