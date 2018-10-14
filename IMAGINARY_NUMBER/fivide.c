ec18c37763903aa59288151b649c5ff5af598b21
RaduGitHub <radu.mihai17@yahoo.com>
2018-10-14 22:20:32 +0300

    Something messy

--- IMAGINARY_NUMBER/fivide.c
+++ IMAGINARY_NUMBER/fivide.c
@@ -5,8 +5,8 @@ void divide(float real_1, float imag_1, float real_2, float imag_2){
 
     float real, imag;
 
-    real = ( real_1 * real_2 + imag_1 * imag_2 ) / ( real_1 * real_1 + imag_1 *imag_2 );
-    imag = ( real_2 * imag_1 - real_1 * imag_2 ) / ( real_1 * real_1 + imag_1 *imag_2 );
+    real = ( (real_1 * real_2) + (imag_1 * imag_2) ) / ( real_1 * real_1 - imag_1 *imag_2 );
+    imag = ( (real_2 * imag_1) - (real_1 * imag_2) ) / ( real_1 * real_1 - imag_1 *imag_2 );
 
     printf("\nReal part after division is: %f", real);
     printf("\nImaginary part after division is: %f", imag);
